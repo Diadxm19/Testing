f6966dd324967b31bbb6d70f9a8887eb85454b72
Diadxm <37122943+Diadxm@users.noreply.github.com>
2019-07-12 21:13:47 -0400

    sadsds
    asdsadsdsa

---
+++ PracticingECS/Assets/Scripts/Testing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Rendering;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private Mesh mesh;
+    [SerializeField] private Material material;
+    private void Start(){
+        EntityManager entityManager = World.Active.EntityManager;
+
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof (LevelComponent),
+             typeof (Translation),
+             typeof (RenderMesh),
+             typeof (LocalToWorld)
+             );
+             NativeArray<Entity> entityArray = new NativeArray<Entity>(1,Allocator.Temp);
+        entityManager.CreateEntity(entityArchetype, entityArray);
+
+        for (int i = 0; i < entityArray.Length; i++){
+            Entity entity = entityArray[i];
+            entityManager.SetComponentData(entity, new LevelComponent{level = Random.Range(0, 2)});
+
+            entityManager.SetSharedComponentData(entity, new RenderMesh{
+                mesh = mesh,
+                material = material
+            });
+        }
+        entityArray.Dispose();
+    }
+}
